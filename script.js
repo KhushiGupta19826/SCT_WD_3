ccc5e0aca33ad0516e114cfaae2ec9aad4be4d0d
KhushiGupta19826 <khushigupta19826@gmail.com>
2025-09-09 09:47:54 +0530

    The design is more visually appealing now and JS in functioning

--- script.js
+++ script.js
@@ -1,37 +1,113 @@
-const boxes = document.querySelectorAll(".box");
-const resetBtn = document.getElementById("reset-btn");
+let boxes = document.querySelectorAll(".box");
+        let resetBtn = document.querySelector("#reset-btn");
+        let newGameBtn = document.querySelector("#new-btn");
+        let msgContainer = document.querySelector(".msg-container");
+        let msg = document.querySelector("#msg");
 
-let turn0 = true; //player X , player 0
-const winPatterns = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
-];
+        let turnO = true; //playerX, playerO
+        let count = 0; //To Track Draw
 
-boxes.forEach((box) => {
-    box.addEventListener("click" , () =>{
-        if(turn0){
-            box.innerText = "O";
-            turn0 = false;
-        }
-        else{
-            box.innerText = "X";
-            turn0 = true;
-        }
-        box.disabled = true;
+        const winPatterns = [
+            [0, 1, 2],
+            [0, 3, 6],
+            [0, 4, 8],
+            [1, 4, 7],
+            [2, 5, 8],
+            [2, 4, 6],
+            [3, 4, 5],
+            [6, 7, 8],
+        ];
 
-        checkWinner();
-    });
-});
+        const resetGame = () => {
+            turnO = true;
+            count = 0;
+            enableBoxes();
+            msgContainer.classList.add("hide");
+        };
 
-const checkWinner = () =>{
-    for(pattern of winPatterns){
-        console.log("check patterns");
-    }
-}
+        boxes.forEach((box) => {
+            box.addEventListener("click", () => {
+                if (turnO) {
+                    //playerO
+                    box.innerText = "O";
+                    box.classList.add("o");
+                    turnO = false;
+                } else {
+                    //playerX
+                    box.innerText = "X";
+                    box.classList.add("x");
+                    turnO = true;
+                }
+                box.disabled = true;
+                count++;
 
+                let isWinner = checkWinner();
+
+                if (count === 9 && !isWinner) {
+                    gameDraw();
+                }
+            });
+        });
+
+        const gameDraw = () => {
+            msg.innerText = `🤝 It's a Draw!`;
+            msgContainer.classList.remove("hide");
+            disableBoxes();
+        };
+
+        const disableBoxes = () => {
+            for (let box of boxes) {
+                box.disabled = true;
+            }
+        };
+
+        const enableBoxes = () => {
+            for (let box of boxes) {
+                box.disabled = false;
+                box.innerText = "";
+                box.classList.remove("x", "o");
+            }
+        };
+
+        const showWinner = (winner) => {
+            msg.innerText = `🎉 Player ${winner} Wins!`;
+            msgContainer.classList.remove("hide");
+            disableBoxes();
+        };
+
+        const checkWinner = () => {
+            for (let pattern of winPatterns) {
+                let pos1Val = boxes[pattern[0]].innerText;
+                let pos2Val = boxes[pattern[1]].innerText;
+                let pos3Val = boxes[pattern[2]].innerText;
+
+                if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+                    if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                        // Highlight winning pattern
+                        boxes[pattern[0]].style.background = "linear-gradient(45deg, #ffd700, #ffed4a)";
+                        boxes[pattern[1]].style.background = "linear-gradient(45deg, #ffd700, #ffed4a)";
+                        boxes[pattern[2]].style.background = "linear-gradient(45deg, #ffd700, #ffed4a)";
+                        
+                        showWinner(pos1Val);
+                        return true;
+                    }
+                }
+            }
+        };
+
+        // Reset winning highlight on new game
+        const resetWinningHighlight = () => {
+            boxes.forEach(box => {
+                box.style.background = "white";
+            });
+        };
+
+        newGameBtn.addEventListener("click", () => {
+            resetWinningHighlight();
+            resetGame();
+        });
+        
+        resetBtn.addEventListener("click", () => {
+            resetWinningHighlight();
+            resetGame();
+        });
